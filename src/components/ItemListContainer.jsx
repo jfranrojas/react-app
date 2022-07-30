fde975ab8b9039619c3a0dc089dfebca0fe4988f
Francisco Rojas <em.franciscorojas.3h1@gmail.com>
2022-07-30 19:19:07 -0300

    creando componentes

---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ItemListContainer({greeting}) {
+  return (
+    <main>
+        <h1 className='titulo'>{greeting}</h1>
+    </main>
+    
+  )
+}
+
+export default ItemListContainer
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import CartWidget from "./CartWidget";
+
+function NavBar(){
+    return(
+       <header className='header'>
+         <nav className='header-nav'>
+            <ul className='nav-ul'>
+                <a href="#" className='ul-li'><li>Home</li></a>
+                <a href="#" className='ul-li'><li>Blog</li></a>
+                <a href="#" className='ul-li'><li>Tienda</li></a>
+                <a href="#" className='ul-li'><li>Contacto</li></a>
+                <a href="#" className='ul-li'><li>Nosotros</li></a>
+                <CartWidget/>
+            </ul>
+         </nav>
+
+       </header> 
+    )
+}
+export default NavBar
