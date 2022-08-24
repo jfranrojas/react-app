367be3bfd00fecc14ddb896f2ae170f86c489052
Francisco Rojas <em.franciscorojas.3h1@gmail.com>
2022-08-23 23:35:31 -0300

    Agreando CartContext

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
+import ItemData from "../../data/data";
+import { useContext } from 'react';
+import { cartContext} from '../../store/CartContext'
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartArrowDown, faFaceSadTear } from '@fortawesome/free-solid-svg-icons';
 import "./Cart.css";
 
 
-function Cart() {
-  return (
-    <div>Cart</div>
-  )
+function Cart(id) {
+    const {cart, removeItem, removeAll, totalPrice, totalAmount} = useContext(cartContext);
+    function RemoveItemCart(){
+        removeItem(id)
+    }
+    if ( cart.lenght === 0){
+        return (
+            <main>
+                <div className='carritoVacioContainer'>
+                    <div className='tituloVacio'>
+                        <h1>Carrito Vacio</h1>
+                        <FontAwesomeIcon className='carritoVacioCara' icon={faFaceSadTear}></FontAwesomeIcon>
+                        <Link to={"/"}><button className='cartResumeButtons'>Ir a Comprar </button></Link>
+                    </div>
+
+                </div>
+            </main>
+        )
+    }
+    
 }
 
 export default Cart
---
+++ src/components/CartItem/CartItem.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faTrashCanArrowUp } from '@fortawesome/free-solid-svg-icons'
+// import ItemCount from '../ItemCount/ItemCount'
+
+function CartItem({img, nombre, precio, cantidad, RemoveItemCart, id, stock}) {
+  return (
+    <div className='itemCart-container'>
+      <img src={img} alt={`Imagen de : ${nombre}`}></img>
+      <div className='description-container'>
+        <h2 className='cartName'>{nombre}</h2>
+        <h3 className='cartAmount'>Cantidad: {cantidad}</h3>
+        <h3 className='cartSubtotal'>Subtotal: {precio}</h3>
+        <h3 className='cartStock'>Stock Disponible: {stock}</h3>
+        <FontAwesomeIcon className='cartDelete' onClick={() => RemoveItemCart(id)} icon={faTrashCanArrowUp}></FontAwesomeIcon>
+      </div>
+    </div>
+  )
+}
+
+export default CartItem
